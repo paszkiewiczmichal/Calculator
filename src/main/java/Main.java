fdbd54c75fe9ba99c7b02c19bc3a4953eede6c1f
Michał <paszkiewicz.michal@gmail.com>
2019-07-24 11:28:26 +0200

    Window class added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
-    
+
+    private JTextField numbersField;
+    private JButton zero, one, two, three, four, five, six, seven, eight, nine, point, ac,  plusMinus, percent,
+            podzielić, razy, minus, plus, bResult;
+
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+    public Window(){
+
+    }
+
 }
