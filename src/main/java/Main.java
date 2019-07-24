c7eae1d9a800a53ca93b83c9c9c760d705f1a208
Michał <paszkiewicz.michal@gmail.com>
2019-07-24 11:30:23 +0200

    JTextFiled added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main extends JFrame implements ActionListener{
 
     private JTextField numbersField;
     private JButton zero, one, two, three, four, five, six, seven, eight, nine, point, ac,  plusMinus, percent,
             podzielić, razy, minus, plus, bResult;
+    private double amount1 = 0, amount2 = 0, result = 0;
+    private int operator = 0;
 
     public static void main(String[] args) {
         Window window = new Window();
@@ -14,6 +18,23 @@ public class Main {
     }
 
     public Window(){
+
+        // Window settings
+
+        setSize(320, 505);
+        setTitle("Calculator");
+        setLayout(null);
+        setResizable(false);
+
+        // Amount field
+
+        numbersField = new JTextField(10);;
+        numbersField.setBounds(0,0,320, 80);
+        numbersField.setFont(new Font("SansSerif", Font.BOLD, 32));
+        numbersField.addActionListener(this);
+        numbersField.setEditable(false);
+        numbersField.setHorizontalAlignment(JTextField.RIGHT);
+        add(numbersField);
 
     }
 
